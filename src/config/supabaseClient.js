015f044718f6cc12dc8fb5c378c7d04b8e1afeca
Codearlo <flavio.cabanah@gmail.com>
2026-02-16 10:19:03 -0500

    sd

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,8 +4,8 @@
 const createClient = window.supabase.createClient;
 
 // Coloca aquí tus credenciales
-const SUPABASE_URL = 'https://gadmnciaxbomkyzqhttb.supabase.co'; 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZG1uY2lheGJvbWt5enFodHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNjUyMDcsImV4cCI6MjA4MDY0MTIwN30.0SfE7TylNqhkBeKM8qWX274WtxEksHfMNoZ7X-oga10'; 
+const SUPABASE_URL = 'https://twawtvmxndjkkmnpquvp.supabase.co'; 
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3YXd0dm14bmRqa2ttbnBxdXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNDk1MzksImV4cCI6MjA4NjgyNTUzOX0.nF_3MCGAu2vE7rQOnQFsVHQ9PkBZpeIjbLkMy2xhXx0'; 
 
 // --- CONFIGURACIÓN DE STORAGE ---
 export const PRODUCTS_BUCKET = 'product-images'; 
